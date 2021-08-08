6368146999224e5426c29e6f47a52d39307e46ca
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-09 03:05:27 +0700

    Update Startup.cs

--- PqSoftware.ABTest/Startup.cs
+++ PqSoftware.ABTest/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PqSoftware.ABTest.Data;
 
 namespace PqSoftware.ABTest
 {
@@ -21,6 +23,8 @@ namespace PqSoftware.ABTest
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
