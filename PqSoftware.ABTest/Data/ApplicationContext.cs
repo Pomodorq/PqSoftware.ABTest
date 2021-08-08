c16463596f4768529517101bb027b179f1accc9c
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-09 03:04:29 +0700

    Create ApplicationContext.cs

---
+++ PqSoftware.ABTest/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PqSoftware.ABTest.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PqSoftware.ABTest.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
