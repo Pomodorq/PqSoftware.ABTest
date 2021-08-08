f7aa168380e4aef08ac5151bc69fe0cd5b75921b
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-09 02:40:05 +0700

    Update Startup.cs

--- PqSoftware.ABTest/Startup.cs
+++ PqSoftware.ABTest/Startup.cs
@@ -27,10 +27,7 @@ namespace PqSoftware.ABTest
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
