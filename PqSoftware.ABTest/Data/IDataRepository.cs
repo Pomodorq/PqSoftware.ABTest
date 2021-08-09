7f22997915ea7738b85222f5ec9a4531a67d2959
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-09 23:51:27 +0700

    Create IDataRepository.cs

---
+++ PqSoftware.ABTest/Data/IDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using PqSoftware.ABTest.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PqSoftware.ABTest.Data
+{
+    public interface IDataRepository
+    {
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+        Task<User> PostUser(User user);
+        Task<IEnumerable<User>> PostUsers(IEnumerable<User> users);
+    }
+}
