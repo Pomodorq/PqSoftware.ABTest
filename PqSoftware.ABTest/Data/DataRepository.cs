db15847997bddd74ca6acf5a6a643a7ff450af58
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-10 00:21:53 +0700

    Create DataRepository.cs

---
+++ PqSoftware.ABTest/Data/DataRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PqSoftware.ABTest.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PqSoftware.ABTest.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly ApplicationContext _context;
+        public DataRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public Task<User> PostUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> PostUsers(IEnumerable<User> users)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
