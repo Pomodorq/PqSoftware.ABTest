ffc12d657644537314f3a9dfbaeaa66ec097887c
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-10 21:45:37 +0700

    Update ApplicationContext.cs

--- PqSoftware.ABTest/Data/ApplicationContext.cs
+++ PqSoftware.ABTest/Data/ApplicationContext.cs
@@ -10,12 +10,18 @@ namespace PqSoftware.ABTest.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<LifetimeCount> LifetimeCounts { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
             //Database.EnsureDeleted();
             //Database.EnsureCreated();
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<LifetimeCount>().HasNoKey();
         }
     }
 }
