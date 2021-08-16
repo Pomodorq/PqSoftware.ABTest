04dd45320e87add6db8b3c1a131b8fdba80d5082
Pomodorq <42971242+Pomodorq@users.noreply.github.com>
2021-08-16 16:42:30 +0700

    Update IUsersLifetimeService.cs

--- PqSoftware.ABTest/Services/IUsersLifetimeService.cs
+++ PqSoftware.ABTest/Services/IUsersLifetimeService.cs
@@ -1,4 +1,5 @@
-﻿using PqSoftware.ABTest.Data.Models;
+﻿using Microsoft.AspNetCore.Http;
+using PqSoftware.ABTest.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace PqSoftware.ABTest.Services
     public interface IUsersLifetimeService
     {
         Task<IList<LifetimeCount>> GetUsersLifetimeDistributionRaw(int projectId);
-        Task<IList<LifetimeIntervalCount>> GetUsersLifetimeDistributionByIntervals(int projectId);
+        Task<IList<LifetimeIntervalCount>> GetUsersLifetimeDistributionByIntervals(int projectId, HttpContext httpContext);
         Task<IList<LifetimeIntervalCount>> GetUsersLifetimeDistributionByRange(int projectId);
     }
 }
